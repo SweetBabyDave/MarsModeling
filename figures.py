a85305f1f187e7a565d2dcff3a9845480cb790d4
SweetBabyDave <SweetBabyDave@gmail.com>
2022-11-17 01:12:48 -0700

    Got the plotting working a little bit

--- figures.py
+++ figures.py
@@ -3,12 +3,33 @@ import numpy as np
 import math
 
 NUM_VARS = 13
-VAR_DICT = {0 : "N Demand (N\u2080)", 1 : "Reactor Volume (V)", 2 : "Fixation Rate (r)", 
-3 : "Bioreactor Harvest Index (TODO)", 4 : "Crop Harvest Index (TODO)", 5 : "Fertilization Efficiency",
-6 : "Digester (AE) Efficiency (TODO)", 7 : "Digester (AN) Efficiency (TODO)", 8 : "Urine Recyclability (TODO)",
-9 : "Fraction Biomass to AE (TODO)", 10 : "Fraction Feces to AE (TODO)", 11 : "Fecal Fraction of Waster (TODO)",
+VAR_DICT = {0 : "N Demand (N\u2080)",
+1 : "Reactor Volume (V)",
+2 : "Fixation Rate (r)",
+3 : "Bioreactor Harvest Index (TODO)",
+4 : "Crop Harvest Index (TODO)",
+5 : "Fertilization Efficiency",
+6 : "Digester (AE) Efficiency (TODO)",
+7 : "Digester (AN) Efficiency (TODO)",
+8 : "Urine Recyclability (TODO)",
+9 : "Fraction Biomass to AE (TODO)",
+10 : "Fraction Feces to AE (TODO)",
+11 : "Fecal Fraction of Waster (TODO)",
 12 : "Crew Members"}
-DEFAULT_VAR = [14, 800, 13, 0.8, 0.5, 1, 0.75, 0.5, 1, 0.8, 0.5, 0.2, 10]
+
+DEFAULT_VAR = [14,
+800,
+13,
+0.8,
+0.5, 
+1,
+0.75,
+0.5,
+1,
+0.8,
+0.5,
+0.2,
+10]
 
 def graph(variables):
     x = np.linspace(0, 365, 365)
@@ -17,16 +38,27 @@ def graph(variables):
     
     # The 1 to start in N_in might be incorrect
     # Need to finish up coding the math for this part. It's super confusing, but I might be able to make it more readable.
-    N_in = (1)*(variables[2])*(variables[1])*(variables[3])*(n_x(variables[9], variables))**(Z)
-    N_out = ((variables[0])/((variables[5])*(variables[4]))) + ((Z)*((variables[0])*(variables[8])*(1-(variables[11])*n_x(variables[10], variables))))
+    N_in = (.015)*(variables[2])*(variables[1])*(variables[3])*(n_x(variables[9], variables))**(Z)
+    print(N_in)
+    N_out = ((variables[0])/((variables[5])*(variables[4]))) + ((Z)*((variables[0])*(variables[8])*(1-(variables[11]))*n_x(variables[10], variables)))
+    print(N_out)
+
     N_rec = variables[0]*((n_x(variables[9], variables))*((1/variables[4])-1)+(variables[11]*(n_x(variables[10], variables)))+(variables[8]*(1-variables[11])))
-    N_change = variables[12] * x * (N_in + N_rec - N_out)
-    print(N_change)
-    print("im here")
-    plt.plot(x, N_change)
+    print(N_rec)
+
+    N_change = (variables[12] * x * (N_in + N_rec - N_out) ) / 14000
+
+    print(N_change / 14000)
+ 
+    plot = plt.figure()
+    plt.plot(x, N_change, marker='o')
+    plt.ylabel("ΔN (kg)")
+    plt.xlabel("Time (d)")
     plt.title("Nitrogen Management Regiment Comparisons")
-    plt.yticks(y)
+    plt.legend(["Combined NFR"])
+    # plt.yticks(y)
     plt.show()
+    plt.savefig("yo.png")
 
 def n_x(x, variables):
     return (((variables[6])*(x))+((variables[7])*(1-(x))))
